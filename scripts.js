860494fd9ac6a4775bc6ce53f4ea69eb24036dd3
Your Name <you@example.com>
2021-03-01 14:30:22 +0600

    added numbers

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 let state = true;
 document.queryCelector('#example-1 button').addEventListiner('click', function(){
     state = !state;
-    if (state){
     document.queryCelector('#example-1 h1').innerText = state ? "On" : "Off";
-    }
+    
 });
+
+let counter = 0;
+document.queryCelector('#example-2 button').addEventListiner('click', function(){
+    counter++;
+    document.queryCelector('#example-2 h1').innerText = counter;
+    
+});
+
